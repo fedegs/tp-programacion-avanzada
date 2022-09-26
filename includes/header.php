6fa4bcf1b30143fdef63b7dcdfa92e5705c49a05
Federico García Savoy <fedegarcia1498@gmail.com>
2022-09-26 15:29:13 -0300

    Add header and footer for includes

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- PICO.CSS -->
+    <link rel="stylesheet" href="../css/pico.min.css">
+    <!-- CUSTOM STYLES -->
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>LibreMercado</title>
+</head>
+<body>
+    <nav class="container-fluid">
+        <ul>
+            <li>
+                <strong>🛍️ LibreMercado</strong>
+            </li>
+        </ul>
+        <ul>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+        </ul> 
+    </nav>
