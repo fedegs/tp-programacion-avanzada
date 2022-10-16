1fac2e6cb5bf7be7b46924cc073799da768097b8
Federico García Savoy <fedegarcia1498@gmail.com>
2022-10-16 20:20:23 -0300

    add img to home page

--- home/index.php
+++ home/index.php
@@ -5,16 +5,21 @@ session_start();
 
 if (!isset($_SESSION["username"])) {
     header('Location: ../login/');
+    exit();
 } else {
     $username = $_SESSION["username"];
 }
 ?>
 
-<main class="container" style="text-align: center;">
-    <article>
-        <h2>Bienvenido a Amaclon, <?php echo $username ?>!</h2>
-        <h4 class="success">¡Ingresaste correctamente!</h4>
-
+<main class="container " style="text-align: center;">
+    <article class="grid" style="padding: 0; overflow: hidden; background-image: linear-gradient(280deg, #bf90f2 0, #8f6cd8 50%, #594bbf 100%);">
+        <div style="padding: 2rem ;">
+             <h2 style="margin-top: 7rem">BIENVENIDO A AMACLON, <?php echo $username; ?>!</h2>
+             <h4>Una nueva forma de vender y comprar.</h4>
+         </div>
+        <div>
+        <img src="../img/123.png">
+        </div>  
     </article>
 </main>
 
