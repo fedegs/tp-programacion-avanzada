71f06cff00383e3ae68af57ed5243fdfac8d17f0
Federico García Savoy <fedegarcia1498@gmail.com>
2022-11-03 23:10:34 -0300

    add first version of update form

--- products/index.php
+++ products/index.php
@@ -1,11 +1,37 @@
 <?php include_once "../includes/header.php" ?>
 
 <?php
-
 include_once "../includes/db.php";
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 $dbInstance = DB::getInstance();
 $conn = $dbInstance->getConnection();
+
+$userId = $_SESSION["user"]["id"];
+
+$sql = "SELECT 
+        p.id, p.title, p.description, p.price, c.name 
+    FROM products p 
+    INNER JOIN categories c 
+    ON p.category_id = c.id 
+    WHERE p.published_by = $userId";
+
+$result = $conn->query($sql);
+
+if ($result) {
+    while ($data = $result->fetch_assoc()) {
+        $products[] = [
+            "title" => $data["title"],
+            "description" => $data["description"],
+            "price" => $data["price"],
+            "category" => $data["name"],
+            "id" => $data["id"]
+        ];
+    }
+}
 
 $result = $conn->query("SELECT id, name FROM categories");
 if ($result) {
@@ -25,6 +51,9 @@ if ($result) {
     <h3>Mis productos</h3>
 
     <table role="grid">
+    <?php if (!isset($products)): ?>
+        <p>No tenés productos publicados :(</p>
+    <?php else: ?>  
         <thead>
             <tr>
                 <th>Título</th>
@@ -35,63 +64,36 @@ if ($result) {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Título prueba</td>
-                <td>Descripción prueba</td>
-                <td>$100</td>
-                <td>Categoría prueba</td>
-                <td>
-                    <a href="">Editar</a>
-                    <a class="warning" href="">Eliminar</a>
-                </td>
-            </tr>
-            <tr>
-                <td>Título prueba</td>
-                <td>Descripción prueba</td>
-                <td>$100</td>
-                <td>Categoría prueba</td>
-                <td>
-                    <a href="">Editar</a>
-                    <a class="warning" href="">Eliminar</a>
-                </td>
-            </tr>
-            <tr>
-                <td>Título prueba</td>
-                <td>Descripción prueba</td>
-                <td>$100</td>
-                <td>Categoría prueba</td>
-                <td>
-                    <a href="">Editar</a>
-                    <a class="warning" href="">Eliminar</a>
-                </td>
-            </tr>
-            <tr>
-                <td>Título prueba</td>
-                <td>Descripción prueba</td>
-                <td>$100</td>
-                <td>Categoría prueba</td>
-                <td>
-                    <a href="">Editar</a>
-                    <a class="warning" href="">Eliminar</a>
-                </td>
-            </tr>
-            <tr>
-                <td>Título prueba</td>
-                <td>Descripción prueba</td>
-                <td>$100</td>
-                <td>Categoría prueba</td>
-                <td>
-                    <a href="">Editar</a>
-                    <a class="warning" href="">Eliminar</a>
-                </td>
-            </tr>
+            <?php
+                foreach ($products as $product) {
+                    $title = $product["title"];
+                    $description = $product["description"];
+                    $price = $product["price"];
+                    $category = $product["category"];
+                    $id = $product["id"];
+                    
+                    $jsFunction = "openEditModal($id, \"$title\", \"$description\", $price, \"$category\")";
+                    
+                    echo "<tr>
+                        <td>$title</td>
+                        <td>$description</td>
+                        <td>$ $price</td>
+                        <td>$category</td>
+                        <td>
+                            <a href='#' onclick='$jsFunction'>Editar</a>
+                            <a class='warning' href='./deleteProductProcess?id=$id'>Eliminar</a>
+                        </td>
+                    </tr>";
+                }
+            ?>
         </tbody>
+    <?php endif; ?>
     </table>
 
     <dialog id="modalProduct">
         <article>
             <header style="padding-bottom: .5rem; margin-bottom: 1rem;">
-                <a id="closeModalProduct" href="" aria-label="Close" class="close"></a>
+                <a id="closeModalProduct" href="#" aria-label="Close" class="close"></a>
                 <h3 style="margin-bottom: .5rem;">Añadir nuevo producto</h3>
             </header>
             <form action="./addProductProcess.php" method="post">
@@ -130,20 +132,89 @@ if ($result) {
         </article>
     </dialog>
 
+    <dialog id="modalEditProduct">
+        <article>
+            <header style="padding-bottom: .5rem; margin-bottom: 1rem;">
+                <a id="closeModalProduct" href="#" aria-label="Close" class="close"></a>
+                <h3 style="margin-bottom: .5rem;">Editando producto</h3>
+            </header>
+            <form action="./addProductProcess.php" method="post">
+                <div class="grid">
+                    <label>
+                        Título
+                        <input name="title" type="text" required />
+                    </label>
+                    <label>
+                        Precio
+                        <input name="price" type="number" step=".01" required />
+                    </label>
+                </div>
+                <label>
+                    Descripción
+                    <textarea style="resize: none;" name="description" id="" cols="60" rows="5"></textarea>
+                </label>
+                <label>
+                    Categoría
+                    <select name="category" required>
+                        <option value="" selected>Select a category…</option>
+                            <?php
+                            foreach ($categories as $category) {
+                                $id = $category["id"];
+                                $name = $category["name"];
+                                echo "<option value=$id>$name</option>";
+                            }                            
+                            ?>
+                    </select>
+                </label>
+                <div class="grid">
+                    <button class="secondary" type="reset">Resetear</button>
+                    <button type="submit">Editar</button>
+                </div>
+            </form>
+        </article>
+    </dialog>
+
 </main>
 
 <script>
-    const btnClose = document.querySelector("#closeModalProduct")
+    const buttonsClose = document.querySelectorAll("#closeModalProduct")
     const btnAddProduct = document.querySelector("#btnAddProduct")
     const modalProduct = document.querySelector("#modalProduct")
+    const modalEditProduct = document.querySelector("#modalEditProduct")
+
+    function openEditModal(id, title, description, price, category) {
+        modalEditProduct.setAttribute("open", true)
+
+        const form = modalEditProduct.querySelector("form")
+        modalEditProduct.querySelector("input[name='title'").value = title
+        modalEditProduct.querySelector("input[name='price'").value = price
+        modalEditProduct.querySelector("textarea[name='description'").value = description
+        const options = modalEditProduct.querySelectorAll("option")
+        options.forEach(opt => {
+            if (opt.getAttribute("value") === "") {
+                opt.removeAttribute("selected")
+            } else {
+                if (opt.text === category) {
+                    opt.setAttribute("selected", true)
+                }
+            }
+        })
+
+        const hiddenInput = document.createElement("input")
+        hiddenInput.setAttribute("type", "hidden")
+        hiddenInput.setAttribute("value", id)
+        form.appendChild(hiddenInput)
+    }
 
     btnAddProduct.addEventListener("click", () => {
         modalProduct.setAttribute("open", true)
     })
 
-    btnClose.addEventListener("click", () => {
+    buttonsClose.forEach((btn) => btn.addEventListener("click", () => {
+        modalEditProduct.removeAttribute("open")
         modalProduct.removeAttribute("open")
-    })
+    }))
+
 </script>
 
 <?php include_once "../includes/footer.php" ?>
